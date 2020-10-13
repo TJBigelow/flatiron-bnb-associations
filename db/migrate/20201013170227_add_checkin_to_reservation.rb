9d3ad5188615d9b0fc0c1c993b9210d8fe3075ce
tjbigelow <tjbigelow@att.net>
2020-10-13 12:12:58 -0500

    Done.

---
+++ db/migrate/20201013170227_add_checkin_to_reservation.rb
@@ -0,0 +1,5 @@
+class AddCheckinToReservation < ActiveRecord::Migration[5.0]
+  def change
+    add_column :reservations, :checkin, :datetime
+  end
+end
---
+++ db/migrate/20201013170238_add_checkout_to_reservation.rb
@@ -0,0 +1,5 @@
+class AddCheckoutToReservation < ActiveRecord::Migration[5.0]
+  def change
+    add_column :reservations, :checkout, :datetime
+  end
+end
